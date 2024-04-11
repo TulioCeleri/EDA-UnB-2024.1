ceb43784d96a261f957f3128751b04afcb9638b3
Túlio Celeri <celeritulio@terra.com.br>
2024-04-10 23:23:59 -0300

    Adicionando resoluções

--- EDA-Lista3/H/f91.c
+++ EDA-Lista3/H/f91.c
@@ -1,7 +1,26 @@
 #include <stdio.h>
 
-int main(){
-    
+int f91(int n){
+    if (n <= 100)
+    {
+        return f91(f91(n + 11));
+    }
+    else
+    {
+        return n - 10;
+    }
+}
 
+int main(void){
+    int n = -1;
+    while (n != 0)
+    {
+        scanf("%d", &n);
+        if (n == 0)
+        {
+            break;
+        }
+        printf("f91(%d) = %d\n", n, f91(n));
+    }
     return 0;
 }
