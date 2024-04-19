9249cb3408d6eaed380de85bf22544c0672b583c
Túlio Celeri <celeritulio@terra.com.br>
2024-04-18 22:43:46 -0300

    Update potencia.c

--- EDA-Lista3/E/potencia.c
+++ EDA-Lista3/E/potencia.c
@@ -1,6 +1,34 @@
 #include <stdio.h>
 
-int main(){
+double CalculaPot(int a, int b){
+    double res;
+    if (b == 0)
+    {
+        return 1;
+    }
+    else if (b > 0)
+    {
+        return a * CalculaPot(a, b - 1);
+    }
+    else 
+    {
+        res = CalculaPot(a , -b);
+        return (1/res);
+    }
+}
 
+int main(){
+    int a, b;
+    scanf("%d %d", &a, &b);
+    
+    if (a == 0 && b <= 0)
+    {
+        printf("indefinido\n");
+    }
+    else
+    {
+        printf("%lf\n", CalculaPot(a, b));
+    }
+    
     return 0;
 }
