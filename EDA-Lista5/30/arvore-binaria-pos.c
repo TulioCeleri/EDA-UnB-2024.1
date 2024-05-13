0f9e73073a6f956c1d8e8fdd6b9a41e3bbab78d8
Túlio Celeri <celeritulio@terra.com.br>
2024-05-13 16:53:06 -0300

    Add exercícios 29, 30, 31

--- EDA-Lista5/30/arvore-binaria-pos.c
+++ EDA-Lista5/30/arvore-binaria-pos.c
@@ -1,16 +1,100 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h> 
 
 typedef struct no {
     int dado;
     struct no *esq, *dir;
 } no;
 
-void pos_ordem (no *raiz){
-    if (raiz != NULL)
+typedef struct pilha {
+    no *dados[100];
+    int topo;
+} pilha;
+
+pilha* criar_pilha(){
+    pilha *p = malloc(sizeof(pilha));
+    p->topo = -1;
+    return p;
+}
+
+void empilhar(pilha *p, no *no){
+    p->dados[++p->topo] = no;
+}
+
+no *desempilhar(pilha *p){
+    if (p->topo == 99)
     {
-        pos_ordem(raiz->esq);
-        pos_ordem(raiz->dir);
-        printf("%d ", raiz->dado);
+        return NULL;
     }
+    return p->dados[p->topo--];
+}
+
+bool pilha_vazia(pilha *p){
+    return p->topo == -1;
+}
+
+void pos_ordem (no *raiz){
+    if (raiz == NULL)
+    {
+        printf("Arvore vazia\n");
+        return;
+    }
+    
+    pilha *p1 = criar_pilha();
+    pilha *p2 = criar_pilha();
+    empilhar(p1,raiz);
+    
+    while (!pilha_vazia(p1))
+    {
+        no *atual = desempilhar(p1);
+        empilhar(p2, atual);
+
+        if (atual->esq != NULL)
+        {
+            empilhar(p1, atual->esq);
+        }
+        if (atual->dir != NULL)
+        {
+            empilhar(p1, atual->dir);
+        }
+    }
+
+    while (!pilha_vazia(p2)){
+        no *aux = desempilhar(p2);
+        printf("%d ", aux->dado);
+    }
+    free(p1);
+    free(p2);
+}
+
+void inserir(no **r, int x){
+    if (*r == NULL)
+    {
+        *r = malloc(sizeof(no));
+        (*r)->dado = x;
+        (*r)->dir = NULL;
+        (*r)->esq = NULL;
+    }
+    else
+    {
+        if (x < (*r)->dado)
+        {
+            inserir(&((*r)->esq), x);
+        }
+        else
+        {
+            inserir(&((*r)->dir), x);
+        }
+    } 
+}
+
+int main(void){
+    no *raiz = NULL;
+    inserir(&raiz, 30);
+    inserir(&raiz, 54);
+    inserir(&raiz, 44);
+    pos_ordem(raiz);
+
+    return 0;
 }
--- EDA-Lista5/31/arvore-binaria-pre.c
+++ EDA-Lista5/31/arvore-binaria-pre.c
@@ -1,17 +1,82 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Definição de um nó da árvore
 typedef struct no {
     int dado;
-    struct no *esq, *dir;
+    struct no* esq;
+    struct no* dir;
 } no;
 
-void pre_ordem (no *raiz){
-    if (raiz != NULL)
-    {
-        printf("%d ", raiz->dado);
-        pre_ordem(raiz->esq);
-        pre_ordem(raiz->dir);
+// Pilha para armazenar os nós da árvore
+typedef struct pilha {
+    struct no* node;
+    struct pilha* next;
+} pilha;
+
+// Função para criar um novo nó da árvore
+no * criar_no(int dado) {
+    no* novo = malloc(sizeof(no));
+    novo->dado = dado;
+    novo->esq = NULL;
+    novo->dir = NULL;
+    return novo;
+}
+
+// Função para empilhar um nó na pilha
+void empilha(pilha** top, no* node) {
+    pilha* novo = malloc(sizeof(pilha));
+    novo->node = node;
+    novo->next = *top;
+    *top = novo;
+}
+
+// Função para desempilhar um nó da pilha
+no* desempilha(pilha** top) {
+    if (*top == NULL) {
+        return NULL;
     }
-    
+    no* desempilhapedNode = (*top)->node;
+    pilha* temp = *top;
+    *top = (*top)->next;
+    free(temp);
+    return desempilhapedNode;
+}
+
+// Função para imprimir a árvore binária em pré-ordem de forma iterativa
+void pre_ordem(no* raiz) {
+    if (raiz == NULL) {
+        return;
+    }
+
+    pilha* stack = NULL;
+    empilha(&stack, raiz);
+
+    while (stack != NULL) {
+        no* currNode = desempilha(&stack);
+        printf("%d ", currNode->dado);
+
+        if (currNode->dir) {
+            empilha(&stack, currNode->dir);
+        }
+        if (currNode->esq) {
+            empilha(&stack, currNode->esq);
+        }
+    }
+}
+
+int main() {
+    // Exemplo de árvore binária
+    no* raiz = criar_no(1);
+    raiz->esq = criar_no(2);
+    raiz->dir = criar_no(3);
+    raiz->esq->esq = criar_no(4);
+    raiz->esq->dir = criar_no(5);
+
+    printf("Árvore binária em pré-ordem: ");
+    pre_ordem(raiz);
+    printf("\n");
+
+
+    return 0;
 }
